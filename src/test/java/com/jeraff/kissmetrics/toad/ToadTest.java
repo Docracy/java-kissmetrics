fa44826cb20fab6062525bc1cc8dcad6738c5db7
Ryan Downing <ryan@toodo.com>
2011-11-18 15:24:32 -0800

    final toad tests

--- src/test/java/com/jeraff/kissmetrics/toad/ToadTest.java
+++ src/test/java/com/jeraff/kissmetrics/toad/ToadTest.java
@@ -7,6 +7,12 @@ import junit.framework.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+/**
+ * Note: these tests only verify that the Toad object is being populated properly via its public methods.  You should
+ * check your kissmetrics live dashboard to make sure the calls are actually going through.  You'll need to set the
+ * KISS_API jvm variable in your runtime configuration in order for the kissmetrics api to actually get called:
+ * -DKISS_API=your_api_key
+ */
 public class ToadTest {
     private static Toad toad;
 
@@ -77,18 +83,54 @@ public class ToadTest {
 
     @Test
     public void testSetSimple() {
-        toad.user("ryan").set(new KissMetricsProperties().put("prop1", "foo"));
+        final KissMetricsProperties props = new KissMetricsProperties().put("prop1", "foo");
+        toad.user("ryan").set(props);
+
+        Assert.assertNotNull(toad.user("ryan").getPropsMap().containsKey("ryan"));
+        Assert.assertEquals(props.getQueryString(), toad.user("ryan").getPropsMap().get("ryan").getQueryString());
+
+        toad.run();
+    }
+
+    @Test
+    public void testSetFluent() {
+        final KissMetricsProperties props = new KissMetricsProperties().put("prop1", "foo");
+        toad.user("ryan").set("prop1", "foo");
+
+        Assert.assertNotNull(toad.user("ryan").getPropsMap().containsKey("ryan"));
+        Assert.assertEquals(props.getQueryString(), toad.user("ryan").getPropsMap().get("ryan").getQueryString());
 
         toad.run();
     }
 
     @Test
     public void testAbort() {
-
+        // check your kissmetrics live feed to make sure this doesn't go through
+        toad.user("ryan").record("blah");
+        toad.abort();
+        toad.run();
     }
 
     @Test
     public void testMixedFluent() {
+        KissMetricsProperties visitedProps = new KissMetricsProperties().put("location", "San Mateo");
+        KissMetricsProperties purchasedProps = new KissMetricsProperties().put("item", "latte").put("cost", 3);
+        KissMetricsProperties userProps = new KissMetricsProperties().put("age", 25).put("hasRewards", true);
 
+        toad.user("ryan").set("age", 25).record("visited").props("location", "San Mateo")
+            .record("purchased").props("item", "latte").props("cost", 3)
+            .set("hasRewards", true)
+            .alias("ryan@toodo.com").alias("sdklfjksd;fh2833332323");
+
+        Assert.assertNotNull(toad.user("ryan").getPropsMap().containsKey("visited"));
+        Assert.assertNotNull(toad.user("ryan").getPropsMap().containsKey("purchased"));
+        Assert.assertNotNull(toad.user("ryan").getPropsMap().containsKey("ryan"));
+        Assert.assertTrue(toad.user("ryan").getAliases().contains("ryan@toodo.com"));
+        Assert.assertTrue(toad.user("ryan").getAliases().contains("sdklfjksd;fh2833332323"));
+        Assert.assertEquals(visitedProps.getQueryString(), toad.user("ryan").getPropsMap().get("visited").getQueryString());
+        Assert.assertEquals(purchasedProps.getQueryString(), toad.user("ryan").getPropsMap().get("purchased").getQueryString());
+        Assert.assertEquals(userProps.getQueryString(), toad.user("ryan").getPropsMap().get("ryan").getQueryString());
+
+        toad.run();
     }
 }
